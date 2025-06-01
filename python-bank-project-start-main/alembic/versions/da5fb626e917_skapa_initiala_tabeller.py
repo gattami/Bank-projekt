8a072abac95169e083c6606922d74bea9659ed72
NadezdaLuneva2024 <luna2004198816@gmail.com>
2025-06-01 21:46:32 +0200

    Initiera Alembic och lägg till första migrationsfilen

---
+++ python-bank-project-start-main/alembic/versions/da5fb626e917_skapa_initiala_tabeller.py
@@ -0,0 +1,22 @@
+"""Skapa initiala tabeller
+
+Revision ID: da5fb626e917
+Revises:
+Create Date: 2025-06-01 21:35:59.169279
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'da5fb626e917'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
